9763afaa7a5c260a5f0d101ae296db56a20cfe19
Claudio <claudiodicaprio89@gmail.com>
2021-11-16 14:38:09 +0100

    palindromicity_done

---
+++ js/script.js
@@ -0,0 +1,27 @@
+// Palidroma Chiedere all’utente di inserire una parola Creare una funzione per capire se la parola inserita è palindrom
+
+
+// function to check str is palindrome or not
+function check_palindrome( string )
+{
+  let c = string.length -1;
+  for( let i = 0 ; i < c/2 ;i++)
+  {
+    let x = string[i] ;//forward character
+    let y = string[c-i];//backward character
+    if( x != y)
+    {
+      // if all character forward and backword won't match then string is not a palindrome.
+      return false;
+    }
+  }
+  /// if all character forward and backword will match then string is a palindrome.
+  return true;
+   }
+
+let ask = prompt('insert a palindrome word');
+let check = check_palindrome(ask);
+console.log(check);
+
+document.querySelector('.here').innerHTML = 'The palindromicity value  for : '+ ask +', is ' + check;
+
